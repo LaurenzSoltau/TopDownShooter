34aee1ee4ff3f276f2fe5bc230917ec148600752
LaurenzSoltau <92428876+LaurenzSoltau@users.noreply.github.com>
2023-05-03 18:07:43 +0200

    drop shadows for drops

--- Assets/xp5.gd
+++ Assets/xp5.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Area2D
 
 @export var xp_value: int = 5
 var player_stats
@@ -8,7 +8,8 @@ func _ready():
 	player_stats = null
 	player_stats = load("res://Assets/Resources/player_stats.tres")
 
-func _on_area_2d_area_entered(area):
+
+func _on_area_entered(area):
 	if area.is_in_group("player"):
 		player_stats.add_stat("xp", xp_value, true)
 		collect_sound.play()
